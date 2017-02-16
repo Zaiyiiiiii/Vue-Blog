eaaad7ea1d2d50939953adb20f3c74d92e2dd427
LanSa <908248012@qq.com>
2017-02-16 16:15:39 +0800

    add

---
+++ src/components/Login.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="login-box-wrapper">
+    <div>this is login box</div>
+  </div>
+</template>
+<style>
+    body{
+        background-color:#ff0000;
+    }
+
+    .login-box-wrapper {
+       height: 100%;
+       width: 100%;
+       position: fixed;
+       top: 0;
+       left: 0;
+       background-color: rgba(47, 47, 47, 0.3)
+    }
+</style>
+<script>
+    export default{
+        data(){
+            return{
+                msg:'hello vue',
+                isShow: this.$parent.isShowLoginBox
+            }
+        },
+    }
+
+</script>
