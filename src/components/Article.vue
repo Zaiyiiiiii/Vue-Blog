f22138c65ef45a874137932c8eba42f3d02d42fe
LanSa <908248012@qq.com>
2017-02-23 18:21:59 +0800

    add Article component

--- src/components/Article.vue
+++ src/components/Article.vue
@@ -1,3 +1,35 @@
 <template>
-    <div>文章详情</div>
+    <dl>
+      <dt><router-link to="{ path: '/abc'}">{{data.title}}</router-link></dt>
+      <dd>
+        <button v-if="" @click="edit">编辑</button>
+        <span>发布时间：{{data.pubTime}}</span>
+        <span>作者：{{data.author}}</span>
+        <button @click="showComment">评论个数({num})</button>
+      </dd>
+    </dl>
 </template>
+
+<style>
+    body{
+        background-color:#ff0000;
+    }
+</style>
+
+<script>
+    export default{
+        props: ['data']
+        data(){
+            return{}
+        },
+        methods:{
+        //编辑
+           edit(){
+             //路由跳转
+           },
+        //展开评论列表
+           showComment(){
+           }
+        },
+    }
+</script>
