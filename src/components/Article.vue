dd7c840250acaaf4e73d22228b40aab3c0c9a913
LanSa <908248012@qq.com>
2017-03-03 14:29:33 +0800

    获取url是否为编辑确定文章状态

--- src/components/Article.vue
+++ src/components/Article.vue
@@ -29,6 +29,7 @@
     import * as AutoList from '../../static/autolist.min.js'
     import * as MediumButton from 'medium-button'
     var autolist = new AutoList();
+
     export default{
         computed:{
         },
@@ -67,6 +68,7 @@
             var editor = new MediumEditor('.article-editor',this.editorConfig)
             document.querySelector(".article-editor").setAttribute("contenteditable",this.ifEdit)
             hljs.initHighlighting();
+ 
         },
         data(){
             return{
@@ -74,7 +76,7 @@
                 fullScreenElement:document.documentElement,
                 title:"fff",
                 articleContext:"32132132132",
-                ifEdit: (document.URL).split("#")[1] == "exit" ? true : false,
+                ifEdit: (document.URL).split("#")[1] == "edit" ? true : false,
                 editorConfig:{
                         activeButtonClass: 'medium-editor-button-active',
                         buttonLabels: false,
