2cafe6d7c1f2d84479a60f18a28577027d8e78df
LanSa <908248012@qq.com>
2017-03-03 16:40:19 +0800

    add

---
+++ src/components/ImgCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="img-card">
+    <a href="">
+      <img :src="content.src"/>
+    </a>
+    <p class="caption-title">{{content.title}}</p>
+    <figcaption class="caption-content">{{content.desc}}</figcaption>
+  </div>
+</template>
+<style>
+   .img-card {
+      width: 300px;
+      height: 500px;
+      padding: 0 6px;
+      box-sizing: border-box;
+   }
+</style>
+<script>
+    export default{
+        props: ["content"],
+    }
+</script>
