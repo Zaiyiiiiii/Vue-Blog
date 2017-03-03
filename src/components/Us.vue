4fa6f9bd283eba51c2e7942e8c267f5fdd8aecdd
LanSa <908248012@qq.com>
2017-03-03 18:19:50 +0800

    add img card

--- src/components/Us.vue
+++ src/components/Us.vue
@@ -1,7 +1,9 @@
 <template>
-    <div class="us">
-        我们
-    </div>
+  <div class="us">
+    <ul>
+      <li class="card-list" v-for="cardInfo in cardsInfo"><img-card :content="cardInfo"/></li>
+    </ul>
+  </div>
 </template>
 <style>
     .us{
@@ -9,4 +11,28 @@
         height:100%;
         width:100%;
     }
+
+    .card-list{
+       display: inline-block;
+    }
+
 </style>
+
+<script>
+  import ImgCards from "./ImgCard"
+
+  export default{
+    components: {
+     'img-card': ImgCards
+    },
+    data(){
+    return {
+      cardsInfo: [
+        {title: "一页知秋", src: "../../img/imgcard/fy.jpg", desc: "图片1"},
+        {title: "title2", src: "../../img/imgcard/fy.jpg", desc: "图片2"},
+        {title: "悬崖", src: "../../img/imgcard/xuanya.jpg", desc: "图片3"}
+      ]
+     }
+    }
+  }
+</script>
