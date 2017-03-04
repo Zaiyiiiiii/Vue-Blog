a5b4a4a3e74ea6e2539c9f302abf42c6a15ebfd6
shayanglan <908248012@qq.com>
2017-03-05 00:39:54 +0800

    增加测试数据

--- src/components/BlogList.vue
+++ src/components/BlogList.vue
@@ -42,13 +42,43 @@
           pubTime: "2016-7-20",
           author: "wang",
           name: "wang",
-          id: "1"
+          id: "1",
+          commentNum: "10"
         }, {
           title: "vue可以使用watch来监听所有的数据变化，比如data, 样式背景等,",
           pubTime: "2016-7-20",
           author: "sha yang lan",
           name: "sha",
-          id: "2"
+          id: "2",
+          commentNum: "122"
+        }, {
+          title: "测试博客列表，第一条的title，vue是双向数据绑定，通过v-model实现，使用props进行父子组件的通信",
+          pubTime: "2016-7-20",
+          author: "wang",
+          name: "wang",
+          id: "1",
+          commentNum: "10"
+        }, {
+          title: "vue可以使用watch来监听所有的数据变化，比如data, 样式背景等,",
+          pubTime: "2016-7-20",
+          author: "sha yang lan",
+          name: "sha",
+          id: "2",
+          commentNum: "122"
+        }, {
+          title: "测试博客列表，第一条的title，vue是双向数据绑定，通过v-model实现，使用props进行父子组件的通信",
+          pubTime: "2016-7-20",
+          author: "wang",
+          name: "wang",
+          id: "1",
+          commentNum: "10"
+        }, {
+          title: "vue可以使用watch来监听所有的数据变化，比如data, 样式背景等,",
+          pubTime: "2016-7-20",
+          author: "sha yang lan",
+          name: "sha",
+          id: "2",
+          commentNum: "122"
         }],
         blogSortData: [
           {
